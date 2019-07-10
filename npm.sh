5411042a1253c2e331ec15066f4d7958ed01059d
Troy Harvey <troyharvey@Troys-MacBook-Pro.local>
2019-07-10 17:33:48 -0400

    summer 2019 cleaning on npm.sh

--- npm.sh
+++ npm.sh
@@ -8,13 +8,8 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-npm install --global git-open
-npm install --global jwt-cli
-npm install --global compare-json-keys
 npm install --global create-react-app
 npm install --global git-open
-npm install --global gulp
-npm install --global hpm-cli
-npm install --global jwt-cli
 npm install --global json
+npm install --global jwt-cli
 npm install --global prettyjson
