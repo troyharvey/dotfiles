fc936bd60a2a7a16efbf6fdab8fe70da0b5ec65a
Troy Harvey <t.harvey@carta.com>
2022-12-24 23:26:03 -0500

    Add zsh aliases

---
+++ .aliases.zsh
@@ -0,0 +1,35 @@
+## SHORTCUTS
+
+alias p="cd ~/Projects"
+alias g="git"
+alias kanban="open https://carta1.atlassian.net/jira/software/c/projects/DA/boards/642?quickFilter=1444&assignee=607eeab27495ff006aee01e5"
+
+# Always use color output for `ls`
+alias ls="command ls --color"
+
+## STRINGS
+
+alias sql="pbpaste | sqlformat - --reindent --indent_width 4 --comma_first True --use_space_around_operators --keywords=lower | pbcopy"
+alias json="pbpaste | jq | pbcopy"
+alias b64e="pbpaste | base64 | pbcopy"
+alias b64d="pbpaste | base64 --decode | pbcopy"
+
+# GIT STUFF
+
+alias g="git"
+alias ga="git add ."
+alias gc="git checkout"
+alias gcam="git commit -am"
+alias gcb="git checkout -b"
+alias gcm="git commit -m"
+alias gf="git fetch"
+alias gp="git pull"
+alias gpo="git push origin"
+alias gs="git status"
+alias GS="git status"
+alias gne="git commit --amend --no-edit"
+alias gu="git reset HEAD~"
+
+# KUBERNETES STUFF
+
+alias kgp="kubectl get pods"
---
+++ .zshrc
@@ -0,0 +1,26 @@
+# Environment variables direnv.net
+eval "$(direnv hook zsh)"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/usr/local/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/usr/local/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
+        . "/usr/local/Caskroom/miniconda/base/etc/profile.d/conda.sh"
+    else
+        export PATH="/usr/local/Caskroom/miniconda/base/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+# Prompt starship.rs
+eval "$(starship init zsh)"
+
+# fzf keybindings and fuzzy completion
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# Aliases
+[ -f ~/.aliases.zsh ] && source ~/.aliases.zsh
