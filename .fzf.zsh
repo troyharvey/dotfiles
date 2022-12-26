8aae1de4240752305cc23322392ce1ac498d1695
Troy Harvey <t.harvey@carta.com>
2022-12-25 21:06:02 -0500

    fzf colors

--- .fzf.zsh
+++ .fzf.zsh
@@ -11,3 +11,10 @@ fi
 # Key bindings
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+# fzf colors https://minsw.github.io/fzf-color-picker/
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+ --color=fg:#a599e9,bg:#2d2b55,hl:#ffee80
+ --color=fg+:#a599e9,bg+:#1e1e3f,hl+:#fad000
+ --color=info:#ff9d00,prompt:#fb94ff,pointer:#fb94ff
+ --color=marker:#a5ff90,spinner:#ff9d00,header:#3AD900'
