c769f7feb4faf652365bd3a4f628d22c7889523f
Troy Harvey <t.harvey@carta.com>
2023-01-30 22:18:01 -0500

    Java and jenv

--- .exports.zsh
+++ .exports.zsh
@@ -13,3 +13,37 @@ export MANPAGER='less -X';
 
 # Always enable colored `grep` output.
 export GREP_OPTIONS='--color=auto';
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('$(brew --prefix)/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$(brew --prefix)/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
+        . "$(brew --prefix)/Caskroom/miniconda/base/etc/profile.d/conda.sh"
+    else
+        export PATH="$(brew --prefix)/Caskroom/miniconda/base/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+# direnv.net (Environment variables)
+eval "$(direnv hook zsh)"
+
+# GNU "make" has been installed as "gmake".
+# If you need to use it as "make", you can add a "gnubin" directory
+# to your PATH from your zshrc like:
+export PATH="$(brew --prefix)/opt/make/libexec/gnubin:$PATH"
+
+# Poetry (Python)
+export PATH="$HOME/.local/bin:$PATH"
+
+# Java
+export PATH="/usr/local/opt/openjdk/bin:$PATH"
+export CPPFLAGS="-I/usr/local/opt/openjdk/include"
+
+# Java (jenv)
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
--- .zshrc
+++ .zshrc
@@ -1,21 +1,3 @@
-# Environment variables direnv.net
-eval "$(direnv hook zsh)"
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('$(brew --prefix)/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "$(brew --prefix)/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
-        . "$(brew --prefix)/Caskroom/miniconda/base/etc/profile.d/conda.sh"
-    else
-        export PATH="$(brew --prefix)/Caskroom/miniconda/base/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-
 # Prompt starship.rs
 eval "$(starship init zsh)"
 
@@ -33,15 +15,9 @@ eval "$(starship init zsh)"
 # fatal: failed to write commit object
 pkill -9 gpg-agent
 
-# GNU "make" has been installed as "gmake".
-# If you need to use it as "make", you can add a "gnubin" directory
-# to your PATH from your zshrc like:
-PATH="$(brew --prefix)/opt/make/libexec/gnubin:$PATH"
-
 # zsh completion
 autoload -Uz compinit && compinit
 
 # zsh config
 HISTSIZE=999999999
 SAVEHIST=$HISTSIZE
-
